33e58b5a334fe5b60a89713314e8a3498126123b
Saaewarna <saaewarna@gmail.com>
2025-04-25 22:17:40 +0700

    inquiry.js

--- api/inquiry.js
+++ api/inquiry.js
@@ -66,6 +66,6 @@ export default async function handler(req, res) {
     res.status(200).json(data);
   } catch (error) {
     console.error('API error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: 'Sedang Maintenance, Coba Lagi Nanti :)' });
   }
 }
